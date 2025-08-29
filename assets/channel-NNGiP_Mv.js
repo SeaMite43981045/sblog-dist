addf2457329cf29257fd51846bd250099934f8b2
SeaMite43981045 <seamite43981045@163.com>
2025-08-29 08:18:21 +0800

    fix: The marquee component was removed to fix the stuttering issue

--- assets/channel-NNGiP_Mv.js
+++
@@ -1,1 +0,0 @@
-import{aV as o,aW as n}from"./index-B2tKk7tm.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ assets/channel-aR7-z7Sb.js
@@ -0,0 +1,1 @@
+import{aV as o,aW as n}from"./index-hRBZqKaB.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
